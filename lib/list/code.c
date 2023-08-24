7f33b498099ba4c1db7dfb2ecf8c4a1e365238c2
RenanSouza2 <renan.rodrigues.souza1@gmail.com>
2023-08-24 10:09:18 -0300

    free functions

--- lib/list/code.c
+++ lib/list/code.c
@@ -15,3 +15,40 @@ list_body_p list_body_create(node_p n, list_body_p lb_next)
     *lb = (list_body_t){n, lb_next};
     return lb;
 }
+
+
+
+void free_list_body_item(list_body_p lb)
+{
+    free(lb);
+}
+
+void free_list_head_item(list_head_p lh)
+{
+    free(lh);
+}
+
+
+
+void free_list_body(list_body_p lb)
+{
+    while(lb != NULL)
+    {
+        list_body_p lb_aux = lb;
+        lb = lb->lb;
+
+        free_list_body_item(lb_aux);
+    }
+}
+
+void free_list_head(list_head_p lh)
+{
+    while(lh != NULL)
+    {
+        list_head_p lh_aux = lh;
+        lh = lh->lh;
+
+        free_list_body(LB(lh_aux)->lb);
+        free_list_head_item(lh_aux);
+    }
+}
